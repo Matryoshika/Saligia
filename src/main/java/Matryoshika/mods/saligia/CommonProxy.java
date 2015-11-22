7283e5aec63ae53a6324d79fe708dd7955250909
Matryoshika <mad.matryoshika@gmail.com>
2015-11-23 00:34:32 +0100

    Common Proxy

---
+++ src/main/java/Matryoshika/mods/saligia/CommonProxy.java
@@ -0,0 +1,36 @@
+package Matryoshika.mods.saligia;
+
+import Matryoshika.mods.saligia.rendering.GUIHandler.MSGuiHandler;
+import Matryoshika.mods.saligia.utils.ConfigHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.IGuiHandler;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import net.minecraftforge.common.config.Configuration;
+
+public class CommonProxy {
+
+	public void registerRenderers(){
+		
+	}
+	
+	public void registerEntities(){
+		
+	}
+	
+	public void initCommon() {
+	       NetworkRegistry.INSTANCE.registerGuiHandler(saligia.MODID, new MSGuiHandler());
+	}
+
+	public void initClient() {
+
+	}
+
+	public void initServer() {
+
+	}
+	
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+}
