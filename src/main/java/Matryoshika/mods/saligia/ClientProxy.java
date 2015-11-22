77c272d2aa45ae43f9e3f56e1a95bb0144f13b02
Matryoshika <mad.matryoshika@gmail.com>
2015-11-23 00:34:47 +0100

    Client Proxy

---
+++ src/main/java/Matryoshika/mods/saligia/ClientProxy.java
@@ -0,0 +1,37 @@
+package Matryoshika.mods.saligia;
+
+import Matryoshika.mods.saligia.entities.EntityAcedia;
+import Matryoshika.mods.saligia.entities.EntityAvaritia;
+import Matryoshika.mods.saligia.entities.EntityGula;
+import Matryoshika.mods.saligia.entities.EntityInvidia;
+import Matryoshika.mods.saligia.entities.EntityIra;
+import Matryoshika.mods.saligia.entities.EntityLuxuria;
+import Matryoshika.mods.saligia.entities.EntitySuperbia;
+import Matryoshika.mods.saligia.rendering.entities.RenderAcedia;
+import Matryoshika.mods.saligia.rendering.entities.RenderAvaritia;
+import Matryoshika.mods.saligia.rendering.entities.RenderGula;
+import Matryoshika.mods.saligia.rendering.entities.RenderInvidia;
+import Matryoshika.mods.saligia.rendering.entities.RenderIra;
+import Matryoshika.mods.saligia.rendering.entities.RenderLuxuria;
+import Matryoshika.mods.saligia.rendering.entities.RenderSuperbia;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import net.minecraft.client.model.ModelBiped;
+
+public class ClientProxy extends CommonProxy {
+	
+	int ModEntityID;
+
+	public void registerRenderers(){
+		
+		RenderingRegistry.registerEntityRenderingHandler(EntityAcedia.class, new RenderAcedia(new ModelBiped(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityAvaritia.class, new RenderAvaritia(new ModelBiped(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityGula.class, new RenderGula(new ModelBiped(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityInvidia.class, new RenderInvidia(new ModelBiped(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityIra.class, new RenderIra(new ModelBiped(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityLuxuria.class, new RenderLuxuria(new ModelBiped(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntitySuperbia.class, new RenderSuperbia(new ModelBiped(), 0));
+		
+	}
+	
+}
