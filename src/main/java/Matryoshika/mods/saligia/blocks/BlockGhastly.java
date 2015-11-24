d75bf44125436109223c82b252a47a404beb86b3
Matryoshika <mad.matryoshika@gmail.com>
2015-11-24 17:31:16 +0100

    Adding GhastlyBlock

---
+++ src/main/java/Matryoshika/mods/saligia/blocks/BlockGhastly.java
@@ -0,0 +1,53 @@
+package Matryoshika.mods.saligia.blocks;
+
+import javax.swing.Icon;
+
+import Matryoshika.mods.saligia.saligia;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRotatedPillar;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+
+public class BlockGhastly extends Block {
+	
+	@SideOnly(Side.CLIENT)
+    private IIcon GhastlySides;
+    @SideOnly(Side.CLIENT)
+    private IIcon GhastlyFace;
+	
+	public BlockGhastly (Block BlockGhastly){
+		super(Material.water);
+		setStepSound(soundTypeStone);
+		//setBlockTextureName(saligia.MODID+":BlockGhastly");
+		setHardness(10);
+		this.setResistance(150);
+		this.setBlockName("BlockGhastly");
+	}
+	@SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta)
+    {
+        return side == 1 ? this.GhastlySides : (side == 0 ? this.GhastlySides : (side != meta ? this.blockIcon : this.GhastlyFace));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister p_149651_1_)
+    {
+        this.blockIcon = p_149651_1_.registerIcon(saligia.MODID+":BlockGhastly");
+        this.GhastlySides = p_149651_1_.registerIcon(saligia.MODID+":BlockGhastlySides");
+    }
+	
+	
+	public boolean isOpaqueCube()
+    {
+    	return false;
+    }
+}
