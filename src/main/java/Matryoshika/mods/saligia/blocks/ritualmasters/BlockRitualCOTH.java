ce8d8f6c3741ae6cfdf8d28777ded5a96617fddd
Matryoshika <mad.matryoshika@gmail.com>
2015-11-26 00:59:05 +0100

    Placing tile with block

--- src/main/java/Matryoshika/mods/saligia/blocks/ritualmasters/BlockRitualCOTH.java
+++ src/main/java/Matryoshika/mods/saligia/blocks/ritualmasters/BlockRitualCOTH.java
@@ -3,12 +3,15 @@ package Matryoshika.mods.saligia.blocks.ritualmasters;
 import java.util.List;
 
 import Matryoshika.mods.saligia.saligia;
+import Matryoshika.mods.saligia.tile.TileRitualCOTH;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public class BlockRitualCOTH extends Block {
 	
@@ -20,4 +23,24 @@ public class BlockRitualCOTH extends Block {
 		this.setResistance(150);
 		this.setBlockName("BlockRitualCOTH");
 	}
+	
+	@Override
+	public TileEntity createTileEntity(World world, int meta){
+	return new TileRitualCOTH();
+	}
+	
+	@Override
+    public boolean hasTileEntity(){
+        return true;
+    }
+	
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int meta){
+	        super.breakBlock(world, x, y, z, block, meta);
+	}
+	
+	@Override
+	public void onBlockAdded(World world, int x, int y, int z){
+        world.setTileEntity(x, y, z, this.createTileEntity(world, world.getBlockMetadata(x, y, z)));
+    }
 }
