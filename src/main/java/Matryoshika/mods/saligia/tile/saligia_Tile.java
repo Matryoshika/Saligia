7e6547ee2b1afbddbd4d33df96edb71265150024
Matryoshika <mad.matryoshika@gmail.com>
2015-11-26 00:58:24 +0100

    Updating Tile Registry

--- src/main/java/Matryoshika/mods/saligia/tile/saligia_Tile.java
+++ src/main/java/Matryoshika/mods/saligia/tile/saligia_Tile.java
@@ -7,20 +7,20 @@ import Matryoshika.mods.saligia.saligia;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.tileentity.TileEntity;
 
-public class matryoshikassinners_Tile {
+public class saligia_Tile {
 
 	public static TileEntity DynamicGenerator;
+	public static TileEntity RitualCOTH;
 	
 	public static List<TileEntity>TileList=new ArrayList<TileEntity>();
 	
 	public static void registerTiles() {
 		
 		//TileList.add(DynamicGenerator = new TileGenerator());
+		TileList.add(RitualCOTH = new TileRitualCOTH());
 		
 		for(TileEntity Tile:TileList){
-			
+			GameRegistry.registerTileEntity(TileRitualCOTH.class, "tileRitualCOTH");
 		}
-		
 	}
-
 }
