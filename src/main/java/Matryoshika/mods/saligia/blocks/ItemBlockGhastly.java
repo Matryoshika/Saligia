0f973303a8fd3e87208d2d888629a1db5fe176a3
Matryoshika <mad.matryoshika@gmail.com>
2015-11-26 10:26:51 +0100

    Moving hardcoded tooltip to .lang dependent

--- src/main/java/Matryoshika/mods/saligia/blocks/ItemBlockGhastly.java
+++ src/main/java/Matryoshika/mods/saligia/blocks/ItemBlockGhastly.java
@@ -9,8 +9,12 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 
 public class ItemBlockGhastly extends ItemBlock{
+	
+	public String tooltip1;
+	public String tooltip2;
 	
 	public ItemBlockGhastly(Block block){
 		super(block);
@@ -19,8 +23,10 @@ public class ItemBlockGhastly extends ItemBlock{
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean bool){
-		list.add(EnumChatFormatting.GRAY + "As ethereal as smoke, this block ");
-		list.add(EnumChatFormatting.GRAY + "may vanish at the slightest touch");
+		tooltip1 = new String(StatCollector.translateToLocal("saligia.blockghastly.tooltip1"));
+		tooltip2 = new String(StatCollector.translateToLocal("saligia.blockghastly.tooltip2"));
+		list.add(EnumChatFormatting.GRAY + tooltip1);
+		list.add(EnumChatFormatting.GRAY + tooltip2);
 	}
 
 }
