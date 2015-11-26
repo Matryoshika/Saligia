7a982db2b64b288d944fac49c64e212e3de807cb
Matryoshika <mad.matryoshika@gmail.com>
2015-11-26 10:19:29 +0100

    Adding ritual-names to .lang

--- src/main/java/Matryoshika/mods/saligia/items/ItemRitualActivator.java
+++ src/main/java/Matryoshika/mods/saligia/items/ItemRitualActivator.java
@@ -17,13 +17,19 @@ import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 public class ItemRitualActivator extends Item {
 	
 	ChatComponentTranslation chatComponent;
+	
+	ChatComponentTranslation ritualname;
+	
 	int activatorCount = 0;
 	public String ritualSelected;
+	public String ritualSelected2;
+	public String tooltip;
 	public boolean badSetup = false;
 	
 	public ItemRitualActivator(ToolMaterial soul){
@@ -42,7 +48,8 @@ public class ItemRitualActivator extends Item {
 	
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par){ 
-		list.add(EnumChatFormatting.DARK_RED + "Terrors of the mind within your grasp");
+		tooltip = new String(StatCollector.translateToLocal("saligia.ritualactivator.tooltip"));
+		list.add(EnumChatFormatting.GRAY + tooltip);
 	}
 	
 	
@@ -55,8 +62,8 @@ public class ItemRitualActivator extends Item {
 		if(player.isSneaking() == true){
 			activator.setItemDamage(activator.getItemDamage()+1);
 			if (world.isRemote == false){
-				chatComponent = (ChatComponentTranslation) new ChatComponentTranslation("Selected Ritual: "+ritualName(activator)).setChatStyle(new ChatStyle().setColor(EnumChatFormatting.WHITE));
-				player.addChatMessage(chatComponent);
+				ritualSelected2 = new String(StatCollector.translateToLocal("saligia.ritual.select"));
+				player.addChatMessage(new ChatComponentTranslation(ritualSelected2+ritualName(activator)).setChatStyle(new ChatStyle().setColor(EnumChatFormatting.WHITE)));
 			}
 			if(activator.getItemDamage() >= 20){
 				activator.setItemDamage(0);
@@ -77,15 +84,16 @@ public class ItemRitualActivator extends Item {
 				Block blockAtPos = world.getBlock(x1, y1, z1);
 				int meta = world.getBlockMetadata(x, y, z);
 				if(blockAtPos == saligia.GhastlyBlock) {
-					for(int[] coords2 : GHASTLY_BLOCKS) {
-						int x2 = x + coords[0];
-						int y2 = y + coords[1];
-						int z2 = z + coords[2];
-						world.addWeatherEffect(new EntityLightningBolt(world, x2, y2, z2));
-						world.setBlock(x, y, z, saligia_Blocks.CentreCOTH);
-						
-						
+					Item bowl = saligia_Items.SoulCrucible;
+						for(int[] coords2 : GHASTLY_BLOCKS) {
+							int x2 = x + coords[0];
+							int y2 = y + coords[1];
+							int z2 = z + coords[2];
+							//world.addWeatherEffect(new EntityLightningBolt(world, x1, y1, z1));
+							world.playSoundAtEntity(player, "mob.enderdragon.growl", 10F, 0.1F);
+							world.setBlock(x, y, z, saligia_Blocks.CentreCOTH);
 					}
+					
 				}
 				else{
 						wrongSetup(player, world);
@@ -96,84 +104,80 @@ public class ItemRitualActivator extends Item {
 		else{
 			return activator;
 		}
-		
-		
-		
-		
 		super.getItemStackDisplayName(activator);
 		return activator;
 	}
 	
 	public String ritualName(ItemStack stack){
 		if (stack.getItemDamage() == 0){
-			ritualSelected = new String("ÅòkSource Of Sin");
+			ritualSelected = "ÅòkSource of Sin";
 		}
 		if (stack.getItemDamage() == 1){
-			ritualSelected = new String("Cognizance of the Hellmouth"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.COTH"));  
 		}
 		if (stack.getItemDamage() == 2){
-			ritualSelected = new String("Roar of the Thundering Skies"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.ROTTS")); 
 		}
 		if (stack.getItemDamage() == 3){
-			ritualSelected = new String("Fountain of the Divine's blood"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.FOTDB"));  
 		}
 		if (stack.getItemDamage() == 4){
-			ritualSelected = new String("Blessings of the Arid Earth"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.BOTAE")); 
 		}
 		if (stack.getItemDamage() == 5){
-			ritualSelected = new String("Gift from the Ocean Grave"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.GFTOG"));
 		}
 		if (stack.getItemDamage() == 6){
-			ritualSelected = new String("Gaze of the Abyss"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.GOTA"));
 		}
 		if (stack.getItemDamage() == 7){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 8){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder"));
 		}
 		if (stack.getItemDamage() == 9){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 10){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 11){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 12){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 13){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 14){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 15){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 16){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 17){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 18){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 19){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		if (stack.getItemDamage() == 20){
-			ritualSelected = new String("Placeholder"); 
+			ritualSelected = new String(StatCollector.translateToLocal("saligia.ritual.placeholder")); 
 		}
 		return ritualSelected;		
 	}
 	
 	public void wrongSetup(EntityPlayer player, World world){
 		if(world.isRemote == false)
-			player.addChatMessage(new ChatComponentTranslation("The ritual-pattern is not correct for the chosen ritual!").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)));
+			player.addChatMessage(new ChatComponentTranslation("saligia.wrongSetup").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)));
 	}
 	
 	
