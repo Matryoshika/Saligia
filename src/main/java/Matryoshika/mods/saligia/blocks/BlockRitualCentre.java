cba86c19dca70d5eb6e0a57db9f8c862c5b08caa
Matryoshika <mad.matryoshika@gmail.com>
2015-11-26 04:21:06 +0100

    Adding textures to BlockRitualCentre

--- src/main/java/Matryoshika/mods/saligia/blocks/BlockRitualCentre.java
+++ src/main/java/Matryoshika/mods/saligia/blocks/BlockRitualCentre.java
@@ -2,8 +2,11 @@ package Matryoshika.mods.saligia.blocks;
 
 import Matryoshika.mods.saligia.saligia;
 import Matryoshika.mods.saligia.items.saligia_Items;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -11,9 +14,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 public class BlockRitualCentre extends Block {
+	
+	@SideOnly(Side.CLIENT)
+    private IIcon BlockRitualCentreTop;
+    @SideOnly(Side.CLIENT)
+    private IIcon BlockRitualCentreSides;
 	
 	public BlockRitualCentre (Block BlockRitualCentre){
 		super(Material.rock);
@@ -23,4 +32,18 @@ public class BlockRitualCentre extends Block {
 		this.setResistance(150);
 		this.setBlockName("BlockRitualCentre");
 	}
+	
+	@SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta){
+        return side == 1 ? this.BlockRitualCentreTop : (side == 0 ? this.blockIcon : (side != meta ? this.blockIcon : this.BlockRitualCentreTop));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister){
+        this.BlockRitualCentreTop = iconRegister.registerIcon(saligia.MODID+":BlockRitualCentreTop");
+        this.blockIcon = iconRegister.registerIcon(saligia.MODID+":BlockRitualCentreSides");
+    }
+	public boolean isOpaqueCube(){
+    	return false;
+    }
 }
