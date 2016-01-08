b014d99b89176505d26cb2a82e1910c93f5201e9
Matryoshika <mad.matryoshika@gmail.com>
2016-01-08 01:43:53 +0100

    SoulObelisk logic

---
+++ src/main/java/Matryoshika/mods/saligia/tile/soulsystem/TileSoulObelisk.java
@@ -0,0 +1,9 @@
+package Matryoshika.mods.saligia.tile.soulsystem;
+
+public class TileSoulObelisk extends TileSoulBrazier{
+
+	@Override
+	public void TESoulBrazier(){
+		this.capacity = (Matryoshika.mods.saligia.utils.math.SoulObeliskMax());
+	}
+}
