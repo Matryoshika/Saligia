af508de7794a0ef5d4bafcc35341e68b23a18b1b
Matryoshika <mad.matryoshika@gmail.com>
2016-01-10 08:33:24 +0100

    tiny cleanup

--- src/main/java/Matryoshika/mods/saligia/CommonProxy.java
+++ src/main/java/Matryoshika/mods/saligia/CommonProxy.java
@@ -3,15 +3,17 @@ package Matryoshika.mods.saligia;
 
 import Matryoshika.mods.saligia.rendering.GUIHandler.MSGuiHandler;
 import Matryoshika.mods.saligia.utils.ConfigHandler;
+import Matryoshika.mods.saligia.utils.RenderHandler;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.IGuiHandler;
 import cpw.mods.fml.common.network.NetworkRegistry;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 
 public class CommonProxy {
 	
 	
-
 	public void registerRenderers(){
 		
 	}
@@ -22,10 +24,12 @@ public class CommonProxy {
 	
 	public void initCommon() {
 	       NetworkRegistry.INSTANCE.registerGuiHandler(saligia.MODID, new MSGuiHandler());
+	       
 	}
 
+	
 	public void initClient() {
-
+		
 	}
 
 	public void initServer() {
