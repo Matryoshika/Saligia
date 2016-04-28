da38a11afff526f139031d6ac06964a097cd92f9
Matryoshika <jonathancarlsson159@gmail.com>
2016-04-28 16:46:16 +0200

    Render the ItemBlock after the model

--- src/main/java/Matryoshika/mods/saligia/ClientProxy.java
+++ src/main/java/Matryoshika/mods/saligia/ClientProxy.java
@@ -1,5 +1,6 @@
 package Matryoshika.mods.saligia;
 
+import Matryoshika.mods.saligia.blocks.saligia_Blocks;
 import Matryoshika.mods.saligia.entities.EntityAcedia;
 import Matryoshika.mods.saligia.entities.EntityAvaritia;
 import Matryoshika.mods.saligia.entities.EntityGula;
@@ -7,7 +8,11 @@ import Matryoshika.mods.saligia.entities.EntityInvidia;
 import Matryoshika.mods.saligia.entities.EntityIra;
 import Matryoshika.mods.saligia.entities.EntityLuxuria;
 import Matryoshika.mods.saligia.entities.EntitySuperbia;
+import Matryoshika.mods.saligia.rendering.itemPaganAltarRenderer;
+import Matryoshika.mods.saligia.rendering.itemSoulNexusRenderer;
+import Matryoshika.mods.saligia.rendering.tileLeyLineCrystalRenderer;
 import Matryoshika.mods.saligia.rendering.tilePaganAltarRenderer;
+import Matryoshika.mods.saligia.rendering.tileSoulNexusRenderer;
 import Matryoshika.mods.saligia.rendering.GUIHandler.HUDRenderHandler;
 import Matryoshika.mods.saligia.rendering.entities.RenderAcedia;
 import Matryoshika.mods.saligia.rendering.entities.RenderAvaritia;
@@ -17,12 +22,17 @@ import Matryoshika.mods.saligia.rendering.entities.RenderIra;
 import Matryoshika.mods.saligia.rendering.entities.RenderLuxuria;
 import Matryoshika.mods.saligia.rendering.entities.RenderSuperbia;
 import Matryoshika.mods.saligia.tile.altars.TilePaganAltar;
+import Matryoshika.mods.saligia.tile.soulsystem.TileLeyLineCrystal;
+import Matryoshika.mods.saligia.tile.soulsystem.TileSoulNexus;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends CommonProxy {
@@ -42,8 +52,14 @@ public class ClientProxy extends CommonProxy {
 		
 		MinecraftForge.EVENT_BUS.register(new HUDRenderHandler());
 		
+		//TESR
 		ClientRegistry.bindTileEntitySpecialRenderer(TilePaganAltar.class, new tilePaganAltarRenderer());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileLeyLineCrystal.class, new tileLeyLineCrystalRenderer());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileSoulNexus.class, new tileSoulNexusRenderer());
 		
+		//ItemBlock
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(saligia_Blocks.SoulNexus), new itemSoulNexusRenderer(new tileSoulNexusRenderer(), new TileSoulNexus()));
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(saligia_Blocks.AltarPagan), new itemPaganAltarRenderer(new tilePaganAltarRenderer(), new TilePaganAltar()));
 	}
 	
 }
