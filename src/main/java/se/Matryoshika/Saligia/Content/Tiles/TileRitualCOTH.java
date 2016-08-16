900b5e9a680c0c1599cd3ff13219ba98f5b5c555
Matryoshika <jonathancarlsson159@gmail.com>
2016-08-16 18:24:36 +0200

    Working on dynamic sounds for the ritual

--- src/main/java/se/Matryoshika/Saligia/Content/Tiles/TileRitualCOTH.java
+++ src/main/java/se/Matryoshika/Saligia/Content/Tiles/TileRitualCOTH.java
@@ -1,5 +1,6 @@
 package se.Matryoshika.Saligia.Content.Tiles;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.block.Block;
@@ -9,6 +10,9 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -28,15 +32,21 @@ public class TileRitualCOTH extends TileEntity implements ITickable, IMultiblock
 	private static final double RANGE = 2F;
 	public int counter = 0;
 	private boolean canWork = false;
+	public static List<Object[]> GHASTS = new ArrayList<Object[]>();
+	int soundPass = 5;
+	int blockToChoose = 0;
 	
 	public TileRitualCOTH(){
 		setName("TileRitualCOTH");
+		for(int i = 16; i < 24; i++){
+			GHASTS.add(RitualRegistry.getMultiblock("Cognizance Of The HellMouth")[i]);
+		}
 	}
 
 	
 	@Override
 	public void update(){
-		//System.out.println("Updating");
+		
 		counter++;
 		if(counter >= 60){
 			counter = 0;
@@ -44,20 +54,28 @@ public class TileRitualCOTH extends TileEntity implements ITickable, IMultiblock
 		}
 		
 		if(!worldObj.isRemote && canWork){
-			System.out.println(canWork);
+			//System.out.println(soundPass);
 			Class<EntityItem> items = EntityItem.class;
 			List<EntityItem> inbox = worldObj.getEntitiesWithinAABB(items, new AxisAlignedBB(new BlockPos(this.pos.getX()-RANGE, this.pos.getY()-RANGE, this.pos.getZ()-RANGE), new BlockPos(this.pos.getX()+RANGE, this.pos.getY()+RANGE, this.pos.getZ()+RANGE)));
 			if(worldObj.isAirBlock(this.pos.up())){
 				for(EntityItem item : inbox){
 					if(!item.isDead && item.getEntityItem() != null && item.getEntityItem().getItem() == Items.WRITABLE_BOOK){
-						if(item.ticksExisted == 5){
-							System.out.println("Found it!");
+						if(item.ticksExisted == soundPass){
+							Object[] coords = GHASTS.get(blockToChoose);
+							int x = (int) coords[0];
+							int y = (int) coords[1];
+							int z = (int) coords[2];
 							
+							playSound(worldObj,this.pos, new BlockPos(x, y, z));
+							soundPass += 20;
+							blockToChoose += 1;
 						}
-						if(item.ticksExisted >= 320){
-							System.out.println("Spawning new Entity");
+						if(item.ticksExisted >= 155){
+							soundPass = 5;
+							blockToChoose = 0;
+							worldObj.playSound(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.enderdragon.growl")), SoundCategory.HOSTILE, 0.5F, 1F);
 							item.setDead();
-							ItemStack stack = new ItemStack(ContentRegistry.dustIngotItem);
+							ItemStack stack = new ItemStack(ContentRegistry.DUST_INGOT);
 							Entity entity = new EntityItem(worldObj, this.pos.getX()+0.5, this.pos.getY()+1.2, this.pos.getZ()+0.5, stack);
 							worldObj.spawnEntityInWorld(entity);
 							entity.motionX = 0;
@@ -68,6 +86,10 @@ public class TileRitualCOTH extends TileEntity implements ITickable, IMultiblock
 				}
 			}
 		}
+	}
+	
+	public static void playSound(World world, BlockPos origin, BlockPos pos){
+		world.playSound(null, origin.getX() + 0.5 + pos.getX(), origin.getY() + 1.5 + pos.getY(), origin.getZ() + 0.5 + pos.getZ(), SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.ghast.warn")), SoundCategory.HOSTILE, 0.5F, 1F);
 	}
 
 	public static String name;
