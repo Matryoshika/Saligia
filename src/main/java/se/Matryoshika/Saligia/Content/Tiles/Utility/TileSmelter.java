cfbf34586a4bb2c900f5d5865c6c8c37ada3d6a0
Matryoshika <jonathancarlsson159@gmail.com>
2016-10-14 15:25:36 +0200

    Finished the Smelter for now

--- src/main/java/se/Matryoshika/Saligia/Content/Tiles/Utility/TileSmelter.java
+++ src/main/java/se/Matryoshika/Saligia/Content/Tiles/Utility/TileSmelter.java
@@ -4,19 +4,17 @@
 package se.Matryoshika.Saligia.Content.Tiles.Utility;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockFurnace;
 import net.minecraft.block.BlockOre;
 import net.minecraft.block.BlockRedstoneOre;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.item.Item;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.FurnaceRecipes;
-import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import se.Matryoshika.Saligia.Content.Tiles.CustomTileClass;
@@ -30,8 +28,10 @@ import se.Matryoshika.Saligia.Content.Tiles.CustomTileClass;
 public class TileSmelter  extends CustomTileClass implements ITickable{
 
 	public static int TIMER = 0;
-	public static final int LIMIT = 2 * 20;
+	public static final int LIMIT = 1 * 20;
 	private final int RADII = 3;
+	
+	List<BlockPos> furnacePos = new ArrayList<BlockPos>();
 
 	@Override
 	public double[] colourScheme() {
@@ -50,16 +50,24 @@ public class TileSmelter  extends CustomTileClass implements ITickable{
 			TIMER = 0;
 			activate();
 		}
+		
+		if(!furnacePos.isEmpty())
+			for(BlockPos pos : furnacePos)
+				speedFurnace(pos);
 	}
 	
 	private void activate(){
 		
+		furnacePos.clear();
+		
 		for(BlockPos pos : BlockPos.getAllInBox(getPos().add(-RADII, -RADII, -RADII), getPos().add(RADII, RADII, RADII))){
-			if(worldObj.getBlockState(pos).getBlock() instanceof BlockOre)
+			Block block = worldObj.getBlockState(pos).getBlock();
+			if(block instanceof BlockOre || block instanceof BlockRedstoneOre)
 				smeltOre(pos);
 			
-			if(worldObj.getBlockState(pos).getBlock() instanceof BlockFurnace)
-				speedFurnace(pos);
+			if(worldObj.getTileEntity(pos) instanceof TileEntityFurnace){		
+				furnacePos.add(pos);
+			}
 		}
 	}
 	
@@ -68,14 +76,61 @@ public class TileSmelter  extends CustomTileClass implements ITickable{
 		if(itemstack != null && !(worldObj.isRemote)){
 			
 			worldObj.setBlockToAir(pos);
-			worldObj.spawnEntityInWorld(new EntityItem(worldObj, getPos().getX()+0.5, getPos().getY()+1.5, getPos().getZ()+0.5, itemstack.copy()));
+			worldObj.spawnEntityInWorld(new EntityItem(worldObj, pos.getX()+0.5, pos.getY()+1.5, pos.getZ()+0.5, itemstack.copy()));
 			
 		}
 		
 	}
+	/*      TileEntityFurnace fields of interest
+	 * 		case 0:
+     *          return this.furnaceBurnTime;
+     *      case 1:
+     *          return this.currentItemBurnTime;
+     *      case 2:
+     *          return this.cookTime;
+     *      case 3:
+     *          return this.totalCookTime;
+	 */
 	
 	private void speedFurnace(BlockPos pos){
+		//System.out.println("Got a furnace");
+		TileEntityFurnace furnace = (TileEntityFurnace) worldObj.getTileEntity(pos);
+		if(furnace == null)
+			return;
 		
+		boolean canSmelt = canFurnaceSmelt(furnace);
+		
+		if(canSmelt){
+			if(worldObj.getBlockState(furnace.getPos()).getBlock() == Blocks.FURNACE)
+				((BlockFurnace) worldObj.getBlockState(furnace.getPos()).getBlock()).setState(true, worldObj, furnace.getPos());
+			furnace.setField(2, Math.min(199, furnace.getField(2) + 9));
+			furnace.setField(0, 1600);
+		}
+		if(!canSmelt){
+			furnace.setField(0, 0);
+			if(worldObj.getBlockState(furnace.getPos()).getBlock() == Blocks.LIT_FURNACE)
+				((BlockFurnace) worldObj.getBlockState(furnace.getPos()).getBlock()).setState(false, worldObj, furnace.getPos());
+		}
+	}
+	
+	private boolean canFurnaceSmelt(TileEntityFurnace furnace){
+		if(furnace.getStackInSlot(0) == null)
+			return false;
+		else {
+			ItemStack itemstack = FurnaceRecipes.instance().getSmeltingResult(furnace.getStackInSlot(0));
+
+			if(itemstack == null)
+				return false;
+
+			if(furnace.getStackInSlot(2) == null)
+				return true;
+
+			if(!furnace.getStackInSlot(2).isItemEqual(itemstack))
+				return false;
+
+			int result = furnace.getStackInSlot(2).stackSize + itemstack.stackSize;
+			return result <= 64 && result <= itemstack.getMaxStackSize();
+		}
 	}
 	
 }
